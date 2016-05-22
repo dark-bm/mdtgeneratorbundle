03c43d99afde45c94ba019f29085f53ae03eb9cd
Chukin Anton <admin@kusumpharm.com>
2016-05-22 15:50:19 +0300

    Generator adaptation for MDT2

--- src/SCM/MDTGeneratorBundle/Command/GenerateDoctrineRESTCommand.php
+++ src/SCM/MDTGeneratorBundle/Command/GenerateDoctrineRESTCommand.php
@@ -268,13 +268,24 @@ EOT
 
     protected function getNamespacePrefix(InputInterface $input, $bundle)
     {
-        $prefix = $input->getOption('api-namespace') ?: strtolower(str_replace(array('\\', '/'), '_', $bundle));
+        $prefix = $input->getOption('api-namespace') ?: strtolower(str_replace(array('\\', '/'), '_', $this->fromCamelCase($bundle)));
 
         if ($prefix && '/' !== $prefix[0]) {
             $prefix = '/'.$prefix;
         }
 
         return $prefix;
+    }
+
+    protected  function fromCamelCase($input) {
+
+        $input = explode("Bundle",$input)[0];
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('-', $ret);
     }
 
 }
