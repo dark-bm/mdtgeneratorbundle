0c615123b01b62805add584356b7f06038a97149
Chukin Anton <admin@kusumpharm.com>
2016-05-26 23:42:41 +0300

    Validation errors handling

--- src/SCM/MDTGeneratorBundle/Controller/MDTController.php
+++ src/SCM/MDTGeneratorBundle/Controller/MDTController.php
@@ -44,4 +44,26 @@ class MDTController extends FOSRestController
         $data     = array_intersect_key($data, $children);
         $request->request->replace($data);
     }
+
+    /**
+     * Collects errors from form
+     *
+     * @param FormInterface $form
+     * @return array
+     */
+    private function getErrorsFromForm(FormInterface $form)
+    {
+        $errors = array();
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+        return $errors;
+    }
 }
